0de16df7cef9954a3722bfbe046203284e358216
krishna sahu <krishnassm420012@gmail.com>
2022-11-07 20:14:08 +0530

    Add cart Compo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import CartItem from './CartItem';
+import Cart from  './Cart';
+
 
 function App() {
   return (
     <div className="App">
       
-      <CartItem />
+      <Cart />
      
     </div>
   );
---
+++ src/Cart.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import CartItem from './CartItem';
+
+class  Cart extends React.Component{
+    render(){
+       return(
+        <div className='cart'>
+            <CartItem/>
+            <CartItem/>
+            <CartItem/>
+        </div>
+
+       );
+
+        
+    }
+}
+    
+
+export default Cart;
